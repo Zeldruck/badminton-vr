c20833953925dd18e4549901a3d67e8b6e0164d0
Zeldruck <felix.jouannneau@gmail.com>
2024-03-01 11:48:51 -0500

    Field events

---
+++ Assets/Scripts/Field/FieldManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldManager : MonoBehaviour
+{
+    [SerializeField] private string shuttlecockTag;
+    [Space]
+    [SerializeField] private Transform[] _colliderRoots;
+
+    private void Start()
+    {
+        for (int i = 0; i < _colliderRoots.Length; i++)
+        {
+            FiledColliderEvent[] filedColliderEvents = _colliderRoots[i].GetComponentsInChildren<FiledColliderEvent>();
+
+            foreach (var colliderEvent in filedColliderEvents)
+            {
+                colliderEvent.shuttlecockTag = shuttlecockTag;
+                colliderEvent.onTriggerSucceed.AddListener(() => SCTouchGround(i + 1, colliderEvent.Type));
+            }
+        }
+    }
+
+    private void SCTouchGround(int side, FiledColliderEvent.EFieldType eventTriggered)
+    {
+        switch (eventTriggered)
+        {
+            case FiledColliderEvent.EFieldType.OffField: break;
+
+            case FiledColliderEvent.EFieldType.InLeftField: break;
+
+            case FiledColliderEvent.EFieldType.InRightField: break;
+
+            case FiledColliderEvent.EFieldType.InFrontField: break;
+
+            case FiledColliderEvent.EFieldType.InBackField: break;
+        }
+    }
+}
---
+++ Assets/Scripts/Field/FiledColliderEvent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FiledColliderEvent : MonoBehaviour
+{
+    public enum EFieldType
+    {
+        OffField,
+        InLeftField,
+        InRightField,
+        InFrontField,
+        InBackField,
+    }
+
+    [SerializeField] private EFieldType _type;
+    public EFieldType Type => _type;
+
+    [HideInInspector] public UnityEvent onTriggerSucceed;
+
+    [HideInInspector] public string shuttlecockTag;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(shuttlecockTag)) return;
+
+        onTriggerSucceed?.Invoke();
+    }
+}
