897139f2c414c31b0331fc6111c6f545bd7bf063
Topaze01 <tanguynoiton@gmail.com>
2024-03-22 14:12:19 -0400

    Progres 22

--- Assets/Scripts/Physique/hitVolant.cs
+++ Assets/Scripts/Physique/hitVolant.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.VFX;
 using static UnityEngine.GraphicsBuffer;
@@ -12,7 +13,7 @@ public class hitVolant : MonoBehaviour
 {
 
     public float coefVelocite = 2;
-    private Vector3 initVelocity;
+    private float initVelocity;
     private Vector3 initRotation;
     private Vector3 velocity;
     private Vector3 posHit;
@@ -22,7 +23,7 @@ public class hitVolant : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        initVelocity = Vector3.zero;
+        initVelocity = 0;
         posHit = Vector3.zero;
         initRotation = Vector3.zero;
         t = 0;
@@ -31,31 +32,55 @@ public class hitVolant : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
         Rigidbody rb = transform.GetComponent<Rigidbody>();
 
-        //Debug.Log("O");
+        //if (collision.gameObject.tag.Equals("raquette") == false) // && (watchHit.ElapsedMilliseconds > 100 || !watchHit.IsRunning)) // On applique le changement de velocite que si l'objet a le tag raquette
+        //{
+            rb.isKinematic = false;
         Debug.Log("A");
-        Debug.Log(other.gameObject);
-        if (other.gameObject.tag.Equals("raquette") == true) // && (watchHit.ElapsedMilliseconds > 100 || !watchHit.IsRunning)) // On applique le changement de velocite que si l'objet a le tag raquette
+        watchHit.Start();
+        initVelocity = 0;
+
+        //}
+    }
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("raquette") == true && 2 * collision.transform.GetComponent<Rigidbody>().velocity.magnitude >= initVelocity)
         {
-            //initVelocity.x = 10f; initVelocity.y = 10f; initVelocity.z = 0f;
+            initVelocity = 2*collision.transform.GetComponent<Rigidbody>().velocity.magnitude;
+            Debug.Log("C");
+        }
+    }
 
-            initRotation = other.transform.rotation.eulerAngles;
-            initVelocity = 3 * other.transform.GetComponent<Rigidbody>().velocity;
-            Collider col = transform.GetComponent<Collider>();
+    private void OnCollisionExit(Collision collision)
+    {
+        Rigidbody rb = transform.GetComponent<Rigidbody>();
 
-            if (initVelocity.magnitude >= 3 && (initVelocity.magnitude >= other.transform.GetComponent<Rigidbody>().velocity.magnitude)) // Si trop lent, physique de base
+        if (collision.gameObject.tag.Equals("raquette") == true ) // && (watchHit.ElapsedMilliseconds > 100 || !watchHit.IsRunning)) // On applique le changement de velocite que si l'objet a le tag raquette
+        {
+            if(watchHit.ElapsedMilliseconds >= 10)
             {
+                //initVelocity.x = 10f; initVelocity.y = 10f; initVelocity.z = 0f;
+
+                initRotation = collision.GetContact(0).normal;
+  
+
+                float oldVelocity = rb.velocity.magnitude;
+                Collider col = transform.GetComponent<Collider>();
+
+
+
                 t = 0;
                 rb.isKinematic = true;
                 Debug.Log("B");
-                watchHit.Start();
-            }
-            else
-            {
-                col.isTrigger = false;
+
+                watchHit.Reset();
+
+
             }
 
 
@@ -66,7 +91,7 @@ public class hitVolant : MonoBehaviour
         {
             Debug.Log("W");
             rb.isKinematic = false;
-            watchHit.Reset();
+            initVelocity = 0;
             //rb.isKinematic = false;
         }
     }
@@ -164,28 +189,24 @@ public class hitVolant : MonoBehaviour
         Rigidbody rb = transform.GetComponent<Rigidbody>();
         if (rb.isKinematic == true)
         {
-            col.isTrigger = true;
-            t += 1f/60;
-            Debug.Log(t);
-            double vxi = Mathf.Sqrt(initVelocity.x * initVelocity.x + initVelocity.z* initVelocity.z);
-            double vyi = initVelocity.y;
+            t += 1f/600;
+            double vxi = Mathf.Abs(initVelocity * Mathf.Sin(Mathf.Deg2Rad * initRotation.y));
+            double vyi = initVelocity * Mathf.Cos(Mathf.Deg2Rad * initRotation.y);
             double vt = 4.5;
             double g = 9.81;
 
             double vx = vxi * vt*vt/ (vxi * g * t + vt*vt);
             double vy = (vt + vyi) * Mathf.Exp((float) (-1 * t* g / vt)) - vt;
 
-            velocity.x = (float) vx * Mathf.Cos(initRotation.x);
+            velocity.x = (float) vx * Mathf.Cos(Mathf.Deg2Rad * initRotation.x);
             velocity.y = (float) vy;
-            velocity.z = (float) vx * Mathf.Cos(initRotation.z);
+            velocity.z = (float) vx * Mathf.Cos(Mathf.Deg2Rad * initRotation.z);
+
+            Debug.Log("Velo");
+            Debug.Log(initRotation);
+            rb.MovePosition(transform.position + velocity/600);
 
 
-            rb.MovePosition(transform.position + velocity/60);
-
-
-        }
-        else if(rb.velocity.magnitude >= 3) { // Si pas lent, passage sur notre physique
-            col.isTrigger = true;
         }
     }
 }
