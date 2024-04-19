ec2d0757b47d9bc411c6cb22376f96940c40a49c
Topaze01 <tanguynoiton@gmail.com>
2024-04-19 12:56:50 -0400

    Ajout mouvement cam corrié

--- Assets/Scripts/moveRotate.cs
+++ Assets/Scripts/moveRotate.cs
@@ -15,13 +15,13 @@ public class moveRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Transform cam = transform.GetChild(3).GetChild(0).GetChild(0).transform;
+        Transform cam = transform.GetChild(0).GetChild(0).transform;
         Vector3 up = cam.transform.up;
         Vector3 forward = cam.transform.forward;
         Vector3 left = Vector3.Cross(up, forward);
         Vector3 right = Vector3.Cross(forward, up);
 
-        Debug.Log(transform.GetChild(3).GetChild(0).name);
+        Debug.Log(transform.GetChild(0).GetChild(0).name);
 
         float angleUp = Vector3.Angle(up, Vector3.up);
         Debug.Log(angleUp);
@@ -34,15 +34,12 @@ public class moveRotate : MonoBehaviour
 
         if (angleUp > 20 && angleLeft < 70)
         {
-            left.Set(left.x, 0, left.z);
-            transform.Translate(left.normalized * 0.05f);
+            transform.Translate(Vector3.left * 0.05f);
         }
 
         if (angleUp > 20 && angleRight < 70)
         {
-            right.Set(right.x, 0, right.z);
-
-            transform.Translate(right.normalized * 0.05f);
+            transform.Translate(Vector3.right * 0.05f);
         }
     }
 }
